e2c91c1675f5ee0fb198b1a72a93142cc4bc04fa
redoprogrammer <redoprogrammer@gmail.com>
2023-03-26 23:49:09 +0700

    import questions from excel format file

---
+++ api/admin/question/import.php
@@ -0,0 +1,101 @@
+<?php
+include_once('../../common.php'); // include file thiết lập chung
+include_once('../middleware.php');
+require_once '../../../vendor/autoload.php';
+
+use \Shuchkin\SimpleXLSX;
+
+$target_dir = "../../../upload/questions/";
+//kiểm tra phương thức truy vấn phải POST hay không
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $allHeader = getallheaders();
+        $jwt = new Middleware;
+        $user = $jwt->decode($allHeader);
+
+        if ($user) {
+
+            $data = json_decode(file_get_contents("php://input", true));
+            if ($_FILES) {  //file excel    
+                $file = $_FILES['file'];
+
+                if ($xlsx = SimpleXLSX::parse($file['tmp_name'])) {
+                    $importedQuestions  = array();
+                    for ($i = 1; $i < count($xlsx->rows()); $i++) {
+                        $index = $xlsx->rows()[$i][0];
+                        $title = $xlsx->rows()[$i][1];
+                        $option_a = $xlsx->rows()[$i][2];
+                        $option_b = $xlsx->rows()[$i][3];
+                        $option_c = $xlsx->rows()[$i][4];
+                        $option_d = $xlsx->rows()[$i][5];
+                        $answer = strtoupper(trim($xlsx->rows()[$i][6]));
+                        $group_id = (int) $xlsx->rows()[$i][7];
+
+
+                        
+
+                        //validate data
+                        if (
+                            strlen(trim($title)) > 0
+                            && strlen(trim($option_a)) > 0 && strlen(trim($option_b)) > 0 && strlen(trim($option_c)) > 0 && strlen(trim($option_d)) > 0
+                            && (strlen(trim($answer)) == 1 && ($answer == 'A' || $answer == 'B' || $answer == 'C' || $answer == 'D'))
+                            && ($group_id >= 1 && $group_id <= 4)
+                        ) {
+
+                            $obj->insert('questions', [
+                                'title' => $title,
+                                'option_a' => $option_a,
+                                'option_b' => $option_b,
+                                'option_c' => $option_c,
+                                'option_d' => $option_d,
+                                'answer' => $answer,
+                                'group_id' => $group_id,
+                                'created_by' => $user['id']
+                            ]);
+                            $check =$obj->getResult();
+                            if($check){
+                                // Thêm một phần tử mới vào mảng
+                                array_push($importedQuestions, array(
+                                    'index'=>$index,
+                                    'title' => $title,
+                                    'option_a' => $option_a,
+                                    'option_b' => $option_b,
+                                    'option_c' => $option_c,
+                                    'option_d' => $option_d,
+                                    'answer' => $answer,
+                                    'group_id' => $group_id
+                                ));
+                            }
+                        }
+                    }
+
+                    if (count($importedQuestions) > 0) {
+                        echo json_encode([
+                            'status' => 201,
+                            'message' => 'Nhập danh sách câu hỏi thành công!',
+                            'questions' => $importedQuestions,
+                            'size'=>count($importedQuestions)
+                        ]);
+                    } else {
+                        echo json_encode([
+                            'status' => 400,
+                            'message' => 'Nhập danh sách câu hỏi thất bại. Vui lòng kiểm tra lại cấu trúc file!'
+                        ]);
+                    }
+                } else {
+                    echo SimpleXLSX::parseError();
+                }
+            }
+        }
+    } catch (Exception $e) {
+        echo json_encode([
+            'code' => 500,
+            'message' => $e->getMessage()
+        ]);
+    }
+} else {
+    echo json_encode([
+        'code' => 403,
+        'message' => 'Truy cập bị từ chối!'
+    ]);
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '62b4eec59d33463a27fb6df0219c4513c7c7f505',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '62b4eec59d33463a27fb6df0219c4513c7c7f505',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'shuchkin/simplexlsx' => array(
+            'pretty_version' => '1.0.19',
+            'version' => '1.0.19.0',
+            'reference' => '978e4bc2c377d4f1e83ad681705d0327a3725ef4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../shuchkin/simplexlsx',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
